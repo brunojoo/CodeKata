eb3944e217587512f25cd9ad717da3b6bd43cb78
brunojoo <bruno.fonesca10@gmail.com>
2021-04-13 17:58:25 +0100

    delete spacing

--- PokerHands/PokerHands/Pair.cs
+++ PokerHands/PokerHands/Pair.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿
+
+
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework.Constraints;
 
@@ -15,7 +18,6 @@ namespace PokerHands
                     if (current == hand[j].Value)
                         return true;
             }
-
             return false;
         }
 
